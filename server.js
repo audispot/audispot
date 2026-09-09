f8b5bf93865a50aebcab028eaf3944765ebb9cfb
audispot <ismaelkea818@gmail.com>
2026-09-09 07:40:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const { sendEmail, safeStr } = require('./emailUtils');
 const subscriptionTransactions = new Map();
 
 const app = express();
+app.set('trust proxy', true)
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
